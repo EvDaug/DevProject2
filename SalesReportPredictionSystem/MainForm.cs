8c7084181f34fb97d4c378fbe797ab6956c0edcc
Jack Bendtsen <101111482@student.swin.edu.au>
2019-09-25 03:00:24 +0000

    Cleanup commit (note: packages folder is ignored)

---
+++ SalesReportPredictionSystem/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesReportPredictionSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            PopulateDataGridView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMonthly_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MonthlyForm form = new MonthlyForm();
+            form.ShowDialog();
+        }
+
+        private void btnWeekly_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WeeklyForm form = new WeeklyForm();
+            form.ShowDialog();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AddForm form = new AddForm();
+            form.ShowDialog();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lbStock.ClearSelection();
+            string searchValue = tbSearch.Text;
+            lbStock.SelectionMode = DataGridViewSelectionMode.FullRowSelect;      
+            if(searchValue!=null)
+            {
+                try
+                {
+                    foreach (DataGridViewRow row in lbStock.Rows)
+                    {
+                        if ((row.Cells[0].Value.ToString().Equals(searchValue))||(row.Cells[1].Value.ToString().Equals(searchValue))||(row.Cells[2].Value.ToString().Equals(searchValue)))
+                        {
+                            row.Selected = true;                             
+                        }
+                    }
+                }
+                catch
+                {
+                }           
+             }
+        }
+        private void PopulateDataGridView()
+        {
+            string[] row0 = { "001", "item1", "brand1",
+                "3", "5" };
+            string[] row1 = { "002", "item2", "brand1",
+                "6", "7" };
+            string[] row2 = { "003", "item3", "brand3",
+                "3", "4" };
+            string[] row3 = { "004", "item4", "brand4",
+                "2", "3" };
+            this.lbStock.Rows.Add(row0);
+            this.lbStock.Rows.Add(row1);
+            this.lbStock.Rows.Add(row2);
+            this.lbStock.Rows.Add(row3);
+        }
+    }
+}
---
+++ SalesReportPredictionSystem/TestDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace SqlTest
+{
+    class Program
+    {
+        public static MySqlConnection Connect()
+        {
+            var conn = new MySqlConnection(@"server=localhost;database=sqltest;uid=root");
+            return conn;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Opening DB...");
+
+            var conn = Connect();
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failure :(\n{0}", ex.Message);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
